a45c856c8fe6aac1061739107db11964ba88acb8
Muky-dev <ti.gabrielfini@gmail.com>
2022-10-20 11:56:33 -0300

    Mais secções e componentes

--- dataSet.js
+++ dataSet.js
@@ -3,30 +3,38 @@ const dataSet = {
   description: 'A description',
   sections: [
     {
-      title: 'English',
+      title: 'Secção 1',
       description: 'A description',
       links: [
         {
           label: 'Link 1',
           url: 'http://example.com',
           // Ícones do FavIcon
-          icon: '<i class="fab fa-twitter text-secondary"></i>',
+          icon: '<i class="fab fa-twitter text-secondary "></i>',
         },
         {
           label: 'Link 2',
           url: 'http://example.com',
           icon: '',
         },
+        {
+          type: 'youtube',
+          url: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+        },
       ],
     },
     {
-      title: 'Portuguese',
+      title: 'Secção 2',
       description: 'A description',
       links: [
         {
           label: 'Link 3',
           url: 'http://example.com',
           icon: '',
+        },
+        {
+          type: 'img',
+          url: 'https://picsum.photos/1280/720',
         },
       ],
     },
@@ -58,20 +66,31 @@ const render = (dataSet) => {
   linksElement.innerHTML = dataSet.sections
     .map((section) => {
       return `
-      <div class="flex-column mb-4">
-      <h3 class="text-secondary">${section.title}</h3>
-      ${section.links
-        .map((link) => {
-          return `<a
-        rel="nofollow noopener"
-        target="_blank"
-        href="${link.url}"
-        class="btn btn-outline-info btn-lg btn-block"
-        >${link.label}</a
-      >
-      `;
-        })
-        .join('')}
+      <div class="mb-4">
+        <h3 class="text-secondary">${section.title}</h3>
+        <div class="d-flex flex-column align-items-center">
+        ${section.links
+          .map((link) => {
+            if (link.type === 'youtube')
+              return `
+              <div class="embed-responsive embed-responsive-16by9 my-2">
+                <iframe class="embed-responsive-item" src="${link.url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>`;
+            if (link.type === 'img')
+              return `
+                <img src="${link.url}" class="img-fluid w-100 my-2" alt="img"/>
+              `;
+            return `<a
+          rel="nofollow noopener"
+          target="_blank"
+          href="${link.url}"
+          class="btn btn-outline-info btn-lg btn-block"
+          >${link.label}</a
+        >
+        `;
+          })
+          .join('')}
+      </div>
     </div>
     `;
     })
